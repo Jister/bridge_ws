db649ef6d4b88299e74e5ca9eb1b5f224a6a9bfb
Jister <chenjiesjtu14@gmail.com>
2016-07-27 20:31:41 +0800

    flow test

--- sonar/src/data_handle.cpp
+++ sonar/src/data_handle.cpp
@@ -6,12 +6,14 @@
 #include "mavros_extras/SonarDistance.h"
 #include "sonar/Sonar_raw.h"
 #include "lidar_lite_ros/Lidarlite.h"
+#include "leddar_one/Leddar.h"
 
 float Front = 0;
 float Back = 0;
 float Left = 0;
 float Right = 0;
 float Up = 0;
+float Down= 0;
 
 void sonarCallback(const sonar::Sonar_raw sonar)
 {
@@ -25,13 +27,19 @@ void lidarCallback(const lidar_lite_ros::Lidarlite msg)
     Up = msg.distance;
 }
 
+void leddarCallback(const leddar_one::Leddar msg)
+{
+    Down = msg.distance.data;
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "data_handle");
   ros::NodeHandle n;
-  ros::Publisher sonar_pub = n.advertise<mavros_extras::SonarDistance>("sonar_send",10);
-  ros::Subscriber sub1 = n.subscribe("sonar_data", 10, sonarCallback);
-  ros::Subscriber sub2 = n.subscribe("lidar_distance", 10, lidarCallback);
+  ros::Publisher sonar_pub = n.advertise<mavros_extras::SonarDistance>("/sonar_send",1);
+  ros::Subscriber sub1 = n.subscribe("/sonar_data", 1, sonarCallback);
+  ros::Subscriber sub2 = n.subscribe("/lidar_distance", 1, lidarCallback);
+  ros::Subscriber sub3 = n.subscribe("/leddar", 1, leddarCallback);
   ros::Rate loop_rate(20);
 
   while(ros::ok())
@@ -42,6 +50,7 @@ int main(int argc, char **argv)
     sonar_msg.sonar_behind = Back;
     sonar_msg.sonar_left = Left;
     sonar_msg.sonar_right = Right;
+    sonar_msg.sonar_down = Down;
     sonar_msg.sonar_up = Up;
     
     sonar_pub.publish(sonar_msg);
